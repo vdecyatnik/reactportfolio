517dab72bfc827b7e11edb5d2d3dad5d2cd1fccc
Val <vdecyatnik@gmail.com>
2021-03-20 15:55:43 -0700

    fixed footer again

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import pic from "../../images/water.jpg";
 
+import "./index.css";
 
 function Home(){
 
 
     return(
        
-        <div className= "container-fluid">
+        <div className= "container">
 
             <img src={pic} className= "img-fluid" alt="..."></img>
         </div>
